9f8b5b8fd4397c6f7cfbde3e04fa969739b2fefd
wjp <1060638479@qq.com>
2025-08-02 14:51:52 +0800

    feat: 完成用户模块管理的开发

--- src/main/java/com/wjp/waicodermotherbackend/WAiCoderMotherBackendApplication.java
+++ src/main/java/com/wjp/waicodermotherbackend/WAiCoderMotherBackendApplication.java
@@ -1,11 +1,14 @@
 package com.wjp.waicodermotherbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
+// 扫描mapper包
+@MapperScan("com.wjp.waicodermotherbackend.mapper")
 public class WAiCoderMotherBackendApplication {
 
     public static void main(String[] args) {
